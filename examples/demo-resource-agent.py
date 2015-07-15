e71136edb78858c6f17fdd38530c79952eaa1eec
Niek J. Bouman <niek.bouman@epfl.ch>
2015-07-15 21:04:02 +0200

    Documentation update

--- examples/demo-resource-agent.py
+++ examples/demo-resource-agent.py
@@ -1,12 +1,3 @@
-In this tutorial we will demonstrate how we can make a resource agent for a PV that interfaces with `commelecd`, the daemon.
-
-## Step 1: The Resource Agent code
-
-We will create a simple resource agent, that waits for an incoming request, and then sends the parameters for a PV advertisment to the daemon.
-
-The source code can be downloaded here: [demo-resource-agent.py](../examples/demo-resource-agent.py). 
-
-```Python
 import json
 import SocketServer
 
@@ -71,26 +62,3 @@ if __name__ == "__main__":
     server = SocketServer.UDPServer((HOST, PORT), JSONReqHandler)
     server.serve_forever()
 
-```
-
-## Step 2: Configuring the daemon
-
-Here, we assume that you have built the project, so that you can run the executable `commelecd`.
-
-First, we will generate a default configuration file, by running
-  
-  commelecd --generate
-
-This creates the file `daemon-cfg.json`. Open this file with a text editor and set "resource-type" to "pv". Also, set the appropriate ip addresses and port numbers.
-
-## Step 3: Running the daemon and the resource agent
-
-Open a terminal and run
-
-  commelecd daemon-cfg.json
-
-Then, in a new terminal, run the Python code
-
-  python demo-resource-agent.py
-
-work in progres...
